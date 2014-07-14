86c0fd333618ab649361800eb028cd2b003b2be9
Maxence <maxence@spotistic.com>
2014-07-15 01:26:45 +0200

    Added: cleaner setup

--- app.js
+++ app.js
@@ -15,49 +15,125 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-// Gif Oracle
-app.post('/gifme', function (req, res) {
-  // Get info from the post
-  var text = req.body.text.split('-c');
-  var gifQuery = text[0];
-  // Get gifs from giphy
-  request('http://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&q=' + gifQuery, function (err, response, body) {
-    if (err) res.send(err);
-    var b = JSON.parse(body), random = Math.floor(Math.random() * (b.data.length - 1)) + 0;
-    var payload;
-    var url = b.data[random].url;
-    var user = req.body.user_name;
-    var caption = text[1];
-    var gifTitle = text[0];
-    if (caption) payload = 'Title: ' + gifTitle + '\nSent By '+ user + '\n~"' + caption + '"\n<' + url + '>';
-    else if (!caption) payload = 'Title: '+ gifTitle + '\nSent By ' + user + '\n<' + url + '>';
-    var options = {
-      url: 'Your web hook integration url token here',
-      method: 'POST',
-      json: {text: payload, username: 'the bot name', icon_emoji: ':an emoji:'}
-    };
-    // Send gifs to slack channel
-    request(options, function (err) {
-      if (err) res.send(err);
-    });
-    res.send('Gif sent.');
-  });
+app.get('/', function (req, res) {
+  res.send('Running');
 });
 
-// Food Oracle
-app.post('/lunch', function (req, res) {
-  res.send(restaurants[getRandomInt(0, restaurants.length)]);
-});
-
-app.post('/help', function (req, res) {
-  var help = '' +
-    '/gifme keyword [-c "your caption"]\nreturns a gif with the keyword\n\n' +
-    '/wheretoeat \nreturns a place to eat';
-  res.send(help);
-});
+/**
+ * Example of body:
+ * token=V6qJflIDP74OWBHaQB70eUTY
+ * team_id=T0001
+ * channel_id=C2147483705
+ * channel_name=test
+ * timestamp=1355517523.000005
+ * user_id=U2147483697
+ * user_name=Steve
+ * text=googlebot: What is the air-speed velocity of an unladen swallow?
+ * trigger_word=googlebot:
+ *
+ * Response must be a JSON:
+ *   {
+ *     "text": "African or European?"
+ *   }
+ */
+app.post('/', securityChecker,
+              parametizer,
+              dispatcher);
 
 app.use(router);
 var port = Number(process.env.PORT || 5000);
 app.listen(port , function () {
   console.log('listining on port', port);
 });
+
+
+var actions = {};
+
+/**
+ * Get a GIF
+ */
+actions.gifme = function (req, res) {
+  var gifQuery = req.action.params.join(' ');
+  // Get gifs from giphy
+  request('http://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&q=' + gifQuery, function (err, response, body) {
+    if (err) return res.send(err);
+    var b = JSON.parse(body);
+    var random = getRandomInt(0, b.data.length - 1);
+    res.send({ text: b.data[random].url });
+  });
+};
+
+/**
+ * Where to go for lunch?
+ */
+actions.lunch = function (req, res) {
+  res.send({ text: restaurants[getRandomInt(0, restaurants.length - 1)] });
+};
+
+/**
+ * What's playing on the jukebox
+ */
+actions.jukebox = function (req, res) {
+  res.send({ text: 'Nicolas Jaar - Mi Mujer' });
+};
+
+/**
+ * Help menu
+ */
+actions.help = function (req, res) {
+  var help = '' +
+    '/majordomo gifme keyword\nreturns a gif with the keyword\n\n' +
+    '/majordomo lunch\nreturns a place to eat';
+  res.send({ text: help });
+};
+
+/**
+ * Checks that the message really comes from our Slack Integration.
+ */
+function securityChecker(req, res, next) {
+  if (req.body.token !== process.env.SLACK_TOKEN) return res.send(403);
+  next();
+}
+
+/**
+ * Transforms the body into usefuls parameters.
+ *
+ * Example of body:
+ *
+ * token=V6qJflIDP74OWBHaQB70eUTY
+ * team_id=T0001
+ * channel_id=C2147483705
+ * channel_name=test
+ * timestamp=1355517523.000005
+ * user_id=U2147483697
+ * user_name=Steve
+ * text=googlebot: What is the air-speed velocity of an unladen swallow?
+ * trigger_word=googlebot:
+ */
+function parametizer(req, res, next) {
+  req.user = {
+    id: req.body.user_id,
+    name: req.body.user_name
+  };
+
+  req.channel = {
+    id: req.body.channel_id,
+    name: req.body.channel_name
+  };
+
+  var words = req.body.text.replace(req.body.trigger_word, '').trim().split(' ');
+  req.action = {
+    name: words[0],
+    params: words.slice(1)
+  };
+
+  next();
+}
+
+/**
+ * Call the function corresponding to the action received.
+ */
+function dispatcher(req, res, next) {
+  if (!actions[req.action.name]) return res.send(404);
+  actions[req.action.name](req, res, next);
+}
